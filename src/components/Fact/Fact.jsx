f7eec49bdc54891f58e091566ebb2f8e7da1f0cb
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2019-07-27 10:20:10 +0530

    Using hooks

--- src/components/Fact/Fact.jsx
+++ src/components/Fact/Fact.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ClipLoader from 'react-spinners/ClipLoader';
 import Draggable from 'react-draggable';
 
@@ -6,36 +6,28 @@ import Info from '../Info/Info';
 
 import r from '../../config/data';
 
-class Fact extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      post: '',
-      isLoaded: false
-    };
+const Fact = () => {
+  const [post, setPost] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
 
-    this.randomFact = this.randomFact.bind(this);
-  }
+  useEffect(() => {
+    async function fetchPost() {
+      let limit;
+      limit = 20;
+      const subReddit = 'facts';
 
-  async componentDidMount() {
-    let limit;
-    limit = 10;
-    const subReddit = 'facts';
+      let posts = [];
+      let post;
+      posts = await r.getSubreddit(subReddit).getHot({ limit: limit });
+      post = posts[Math.floor(Math.random() * limit - 1) + 1];
+      setPost(post);
+      setIsLoaded(true);
+    }
+    fetchPost();
+  }, []);
 
-    let posts = [];
-    let post;
-    posts = await r.getSubreddit(subReddit).getHot({ limit: limit });
-    post = posts[Math.floor(Math.random() * limit - 1) + 1];
-    this.setState({
-      post,
-      isLoaded: true
-    });
-  }
-
-  async randomFact() {
-    this.setState({
-      isLoaded: false
-    });
+  async function randomFact() {
+    setIsLoaded(false);
     let limit, num;
     limit = 100;
     const subReddit = 'facts';
@@ -82,60 +74,54 @@ class Fact extends Component {
         return;
     }
 
-    this.setState({
-      post,
-      isLoaded: true
-    });
+    setPost(post);
+    setIsLoaded(true);
   }
 
-  render() {
-    const { post, isLoaded } = this.state;
-
-    if (!isLoaded) {
-      return (
-        <div>
+  if (!isLoaded) {
+    return (
+      <div>
+        <div className="max-w-sm mx-auto flex p-4 bg-white rounded-lg shadow-lg mt-48 mb-16">
+          <div className="mx-auto pt-1">
+            <ClipLoader
+              sizeUnit={'px'}
+              size={30}
+              color={'#0076FF'}
+              loading={!isLoaded}
+            />
+          </div>
+        </div>
+        <Info />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Draggable>
           <div className="max-w-sm mx-auto flex p-4 bg-white rounded-lg shadow-lg mt-48 mb-16">
-            <div className="mx-auto pt-1">
-              <ClipLoader
-                sizeUnit={'px'}
-                size={30}
-                color={'#0076FF'}
-                loading={!this.state.isLoaded}
-              />
+            <div className="ml-6 pt-1">
+              <h4 className="text-xl text-gray-900 leading-tight">
+                {post.title}
+              </h4>
+              <button
+                style={{ backgroundColor: '#0076FF' }}
+                className="hover:shadow-lg text-white font-medium py-2 px-4 border border-gray-400 rounded-lg shadow mt-8"
+                onClick={randomFact}
+              >
+                Random Fact
+              </button>
+              <button className="bg-transparent hover:bg-blue-100 hover:border-transparent hover:shadow-lg text-blue-600 font-medium py-2 px-4 border border-blue-400 rounded-lg shadow mt-8 ml-6">
+                <a href={post.url} target="blank">
+                  View Submission
+                </a>
+              </button>
             </div>
           </div>
-          <Info />
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <Draggable>
-            <div className="max-w-sm mx-auto flex p-4 bg-white rounded-lg shadow-lg mt-48 mb-16">
-              <div className="ml-6 pt-1">
-                <h4 className="text-xl text-gray-900 leading-tight">
-                  {post.title}
-                </h4>
-                <button
-                  style={{ backgroundColor: '#0076FF' }}
-                  className="hover:shadow-lg text-white font-medium py-2 px-4 border border-gray-400 rounded-lg shadow mt-8"
-                  onClick={this.randomFact}
-                >
-                  Random Fact
-                </button>
-                <button className="bg-transparent hover:bg-blue-100 hover:border-transparent hover:shadow-lg text-blue-600 font-medium py-2 px-4 border border-blue-400 rounded-lg shadow mt-8 ml-6">
-                  <a href={post.url} target="blank">
-                    View Submission
-                  </a>
-                </button>
-              </div>
-            </div>
-          </Draggable>
-          <Info />
-        </div>
-      );
-    }
+        </Draggable>
+        <Info />
+      </div>
+    );
   }
-}
+};
 
 export default Fact;
